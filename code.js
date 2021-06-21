15f687b5bd819dd879e963c8b264d2f4e755dc08
kryan9829 <kpryan4@gmail.com>
2021-06-21 12:03:24 -0600

    Finished and Removed Excess Code

--- code.js
+++ code.js
@@ -99,59 +99,6 @@ document.body.prepend(rollButton)
 
 thousandButton.addEventListener("click", function(){
     while (counter < 1000){
-        // function rollDice(){
-        //     let rollDiceTotal = rollDiceResults();
-        //     rollResults.innerText= `You rolled a ${rollDiceTotal}!`
-        //     console.log(rollDiceTotal)
-        //     results[rollDiceTotal] +=1
-        //     console.log (results)
-        //     if (rollDiceTotal === 2){
-        //         let two = document.getElementById("2")
-        //         two.innerText = `You have rolled a ${2} \n ${results[2]} times.`
-        //     }
-        //     if (rollDiceTotal === 3){
-        //         let three = document.getElementById("3")
-        //         three.innerText = `You have rolled a ${3} \n ${results[3]} times.`
-        //     }
-        //     if (rollDiceTotal === 4){
-        //         let four = document.getElementById("4")
-        //         four.innerText = `You have rolled a ${4} \n ${results[4]} times.`
-        //     }
-        //     if (rollDiceTotal === 5){
-        //         let five = document.getElementById("5")
-        //         five.innerText = `You have rolled a ${5} \n ${results[5]} times.`
-        //     }
-        //     if (rollDiceTotal === 6){
-        //         let six = document.getElementById("6")
-        //         six.innerText = `You have rolled a ${6} \n ${results[6]} times.`
-        //     }
-        //     if (rollDiceTotal === 7){
-        //         let seven = document.getElementById("7")
-        //         seven.innerText = `You have rolled a ${7} \n ${results[7]} times.`
-        //     }
-        //     if (rollDiceTotal === 8){
-        //         let eight = document.getElementById("8")
-        //         eight.innerText = `You have rolled a ${8} \n ${results[8]} times.`
-        //     }
-        //     if (rollDiceTotal === 9){
-        //         let nine = document.getElementById("9")
-        //         nine.innerText = `You have rolled a ${9} \n ${results[9]} times.`
-        //     }
-        //     if (rollDiceTotal === 10){
-        //         let ten = document.getElementById("10")
-        //         ten.innerText = `You have rolled a ${10} \n ${results[10]} times.`
-        //     }
-        //     if (rollDiceTotal === 11){
-        //         let eleven  = document.getElementById("11")
-        //         eleven.innerText = `You have rolled a ${11} \n ${results[11]} times.`
-        //     }
-        //     if (rollDiceTotal === 12){
-        //         let twelve = document.getElementById("12")
-        //         twelve.innerText = `You have rolled a ${12} \n ${results[12]} times.`
-        //     }
-            
-        //     counter++
-        // }
         rollDice()
         counter++
     }
